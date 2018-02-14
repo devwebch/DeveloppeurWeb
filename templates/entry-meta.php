24d617130d621132051e7ee3dac382aefa41370f
Simon Rapin <simon.rapin@gmail.com>
2018-02-14 16:30:01 +0100

    - updated : post meta

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,1 +1,3 @@
-<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+<div class="entry__meta">
+    <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+</div>
