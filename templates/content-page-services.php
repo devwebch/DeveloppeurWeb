9ed716b5e8c4b0b2566ed94cc4ca74264d1492c6
Simon Rapin <simon.rapin@gmail.com>
2018-02-19 16:58:55 +0100

    - updated: services page to use the_content instead of hardcoded form ID

--- templates/content-page-services.php
+++ templates/content-page-services.php
@@ -1,62 +1,60 @@
 <section class="section section--content section--services">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="icon-block">
-                            <i class="fab fa-wordpress"></i>
-                            <div class="icon-block__content">
-                                <h3>Sites web</h3>
-                                <p>Augmentez la visibilité de votre entreprise grâce à un site vitrine ou vendez vos produits en ligne via une plateforme e-commerce.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="icon-block">
-                            <i class="fab fa-android"></i>
-                            <div class="icon-block__content">
-                                <h3>Applications mobiles</h3>
-                                <p>Vos projets d'applications Android et IOS sont également à portée de main, obtenez l'attention de vos clients sur smartphones et tablettes.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="icon-block">
-                            <i class="fas fa-rocket"></i>
-                            <div class="icon-block__content">
-                                <h3>Systèmes de gestions</h3>
-                                <p>Vous avez essayé tous les systèmes de gestion usuels et rien ne convient ? Nous pouvons vous mettre en place un système sur-mesure.</p>
-                            </div>
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="icon-block">
+                        <i class="fab fa-wordpress"></i>
+                        <div class="icon-block__content">
+                            <h3>Sites web</h3>
+                            <p>Augmentez la visibilité de votre entreprise grâce à un site vitrine ou vendez vos produits en ligne via une plateforme e-commerce.</p>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="icon-block">
-                            <i class="fas fa-mobile"></i>
-                            <div class="icon-block__content">
-                                <h3>Polyvalence</h3>
-                                <p>Internet évolue constamment et le nombre de périphériques sur lesquels votre site web doit être accessible grandis chaque jour.</p>
-                            </div>
+                <div class="col-md-4">
+                    <div class="icon-block">
+                        <i class="fab fa-android"></i>
+                        <div class="icon-block__content">
+                            <h3>Applications mobiles</h3>
+                            <p>Vos projets d'applications Android et IOS sont également à portée de main, obtenez l'attention de vos clients sur smartphones et tablettes.</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="icon-block">
-                            <i class="fas fa-lock"></i>
-                            <div class="icon-block__content">
-                                <h3>Sécurité</h3>
-                                <p>Votre projet bénéficie des dernières normes en terme de sécurité et s'assure une longévité prolongée grâce à la maintenance technique.</p>
-                            </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-block">
+                        <i class="fas fa-rocket"></i>
+                        <div class="icon-block__content">
+                            <h3>Systèmes de gestions</h3>
+                            <p>Vous avez essayé tous les systèmes de gestion usuels et rien ne convient ? Nous pouvons vous mettre en place un système sur-mesure.</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="icon-block">
-                            <i class="far fa-life-ring"></i>
-                            <div class="icon-block__content">
-                                <h3>Support</h3>
-                                <p>Vos solutions seront déployées et maintenues par nos soins, nous entretenons nos créations sur le long terme afin d'assurer leur bon fonctionnement.</p>
-                            </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="icon-block">
+                        <i class="fas fa-mobile"></i>
+                        <div class="icon-block__content">
+                            <h3>Polyvalence</h3>
+                            <p>Internet évolue constamment et le nombre de périphériques sur lesquels votre site web doit être accessible grandis chaque jour.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-block">
+                        <i class="fas fa-lock"></i>
+                        <div class="icon-block__content">
+                            <h3>Sécurité</h3>
+                            <p>Votre projet bénéficie des dernières normes en terme de sécurité et s'assure une longévité prolongée grâce à la maintenance technique.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-block">
+                        <i class="far fa-life-ring"></i>
+                        <div class="icon-block__content">
+                            <h3>Support</h3>
+                            <p>Vos solutions seront déployées et maintenues par nos soins, nous entretenons nos créations sur le long terme afin d'assurer leur bon fonctionnement.</p>
                         </div>
                     </div>
                 </div>
@@ -64,16 +62,14 @@
         </div>
     </div>
 </section>
-<section class="section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h2 class="section__title">Vous avez des questions ?</h2>
-                <p class="section__introduction">N’hésitez pas à nous contacter en cas de questions ou pour un devis gratuit.</p>
-            </div>
-            <div class="col-md-6">
-                <?php echo do_shortcode('[wpforms id="276"]'); ?>
-            </div>
+<section class="section section--content">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="section__title">Vous avez des questions ?</h2>
+            <p class="section__introduction">N’hésitez pas à nous contacter en cas de questions ou pour un devis gratuit.</p>
+        </div>
+        <div class="col-md-6">
+            <?php the_content(); ?>
         </div>
     </div>
 </section>
